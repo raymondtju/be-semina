39b71c66dc783fce5f6e139b5d84994a1d4682cb
raymondtju <raymond.rtju@gmail.com>
2023-03-01 12:19:26 +0700

    mongo live

--- apps/config.js
+++ apps/config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
+  urlDb: process.env.URL_MONGODB,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiration: process.env.JWT_EXPIRATION,
   emailAddress: process.env.EMAIL_ADDRESS,
